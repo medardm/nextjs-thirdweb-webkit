a2fd353597a2254071119730e6d9f00759c49ba7
Medard Mandane <medardm13@gmail.com>
2023-11-12 17:34:06 +0800

    Setup jest

---
+++ src/tests/jest/users.test.ts
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom'
+import {afterAll, beforeAll, describe, expect, it} from "@jest/globals";
+
+beforeAll(async () => {
+})
+
+afterAll(async () => {
+})
+
+describe('Users', () => {
+  it('deletes test users before starting tests', async () => {
+    expect(true).toBeTruthy()
+  })
+})
