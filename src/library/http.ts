610ee8c9e6ff60218fc0a4a070df9b8d6344c758
Medard Mandane <medardm13@gmail.com>
2023-11-17 01:49:19 +0800

    Add error handling in core http library

--- src/library/http.ts
+++ src/library/http.ts
@@ -3,13 +3,23 @@ import {StatusCodes} from "http-status-codes";
 import formidable from "formidable";
 
 export const executeRouteAction = async (actions: any, req: NextApiRequest, res: NextApiResponse) => {
-
   if (!req.method || !actions[req.method]) {
     const responseStatus = getHttpStatus('METHOD_NOT_ALLOWED')
-    return res.status(responseStatus.code).json({message: responseStatus.phrase, success: false})
+    return res
+      .status(responseStatus.code)
+      .json({message: responseStatus.phrase, success: false})
   }
 
-  return await actions[req.method](req, res)
+  let response
+  try {
+    response = await actions[req.method](req, res)
+  } catch (e: any) {
+    response = res
+      .status(getHttpStatus('INTERNAL_SERVER_ERROR').code)
+      .json({error: e, success: false})
+  }
+
+  return response
 }
 
 export const parseFormData = async (req: NextApiRequest): Promise<{ primary: any, files: any }> => {
