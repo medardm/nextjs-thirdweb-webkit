b9017ba5cdfecc97a7ab7718b552f5f60529fb64
Medard Mandane <medardm13@gmail.com>
2023-11-22 00:15:06 +0800

    Add login method to userModel and thirdweb auth route

--- src/library/models/user.model.ts
+++ src/library/models/user.model.ts
@@ -3,6 +3,22 @@ import {Prisma, User} from "@prisma/client";
 
 const userModel = {
   ...prisma.user,
+  login: async (data: {
+    walletAddress: string,
+  }): Promise<User> => {
+    return prisma.user.upsert({
+      where: {
+        walletAddress: data.walletAddress
+      },
+      update: {
+        lastLoginAt: new Date(Date.now()).toISOString()
+      },
+      create: {
+        walletAddress: data.walletAddress,
+        lastLoginAt: new Date(Date.now()).toISOString()
+      },
+    })
+  },
   first: async (value: any, column: keyof User = 'id'): Promise<User | null> => {
     return prisma.user.findFirst({
       where: {
