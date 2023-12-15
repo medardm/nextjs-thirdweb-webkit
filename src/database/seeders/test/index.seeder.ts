235e560b6faf7e37c383f5dce724b2049b2fcf20
Medard Mandane <medardm13@gmail.com>
2023-12-15 22:58:16 +0800

    Add a way to choose which tables to truncate or seed
    - fix GUARDS (change to GuardEnum) imports in make:api templates

--- src/database/seeders/test/index.seeder.ts
+++ src/database/seeders/test/index.seeder.ts
@@ -1,5 +1,21 @@
 import seedTestUsers from "@/database/seeders/test/user.seeder";
+import {TableNames} from "@/database/refreshDatabase";
+import {Prisma} from ".prisma/client";
 
-export default async function seedDevData() {
-  await seedTestUsers()
+export default async function seedDevData(table?: TableNames) {
+  const seeders: any = {
+    [Prisma.ModelName.User]: seedTestUsers,
+  }
+
+  if (table) {
+    // if a specific table is provided, only run the related seeder
+    await seeders[table as any]();
+  } else {
+    // otherwise run all seeders
+    for (const seederName in seeders) {
+      if (seeders.hasOwnProperty(seederName)) {
+        await seeders[seederName]();
+      }
+    }
+  }
 }
